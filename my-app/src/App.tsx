52811389595ab72666004583b6c6e6fac3e65abe
dhaDang <130107069+dhaDang@users.noreply.github.com>
2024-10-14 14:00:58 -0700

    added navbar file (1.4)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,16 +2,18 @@ import "./App.css";
 import { ToDoList } from "./toDoList";
 import { Route, Routes } from "react-router-dom";
 import { StickyNotes } from "./stickyNotes";
+import { Navbar } from "./navbar";
 
 const App = () => {
  return (
-   <div>
-     <Routes>
-       <Route path="/" element={<StickyNotes />} />
-       <Route path="/todolist" element={<ToDoList />} />
-     </Routes>
-   </div>
- );
+    <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<StickyNotes />} />
+      <Route path="/todolist" element={<ToDoList />} />
+    </Routes>
+  </div>
+  );
 };
 
 export default App;
